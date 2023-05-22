bb678885967cd389a53c02e4cbc9170e1a324634
bobbyliyz <yuanzhe_li@mynbps.org>
2023-05-22 08:48:31 -0400

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -17,7 +17,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Hashtable;
